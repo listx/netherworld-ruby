5b7926d3df94ce536ad10231105153e6f7df4861
Linus Arver <linusarver@gmail.com>
2014-08-09 04:41:50 -0700

    refactor
    Move out game loop into its own method in the new 'game' module.
    
    Also, move GameConfig and Player classes into the GameState class.

--- src/main.rb
+++ src/main.rb
@@ -1,57 +1,9 @@
 #!/usr/bin/env ruby
 
-require_relative 'config'
-require_relative 'battle'
-require_relative 'map'
-require_relative 'player'
-require_relative 'random'
-require_relative 'state'
+require_relative 'game'
 
-# Import game configuration.
-game_conf = GameConfig.new(ARGF.filename)
-gs = GameState.new(game_conf)
-p = Player.new(gs.map.first_coord)
+# Import game configuration with path to configuration file. This is the same as
+# starting a new game.
+gs = GameState.new(ARGF.filename)
 
-while true
-	puts gs.map.mini_map(p.coord, 10)
-	puts p.coord_str
-	cmd = STDIN.gets.chomp
-	coord_old = p.coord
-	case cmd
-	when "q"
-		break
-	when "e"
-		p.move(:east, gs.map)
-	when "w"
-		p.move(:west, gs.map)
-	when "n"
-		p.move(:north, gs.map)
-	when "s"
-		p.move(:south, gs.map)
-	when "ne"
-		p.move(:northeast, gs.map)
-	when "nw"
-		p.move(:northwest, gs.map)
-	when "se"
-		p.move(:southeast, gs.map)
-	when "sw"
-		p.move(:southwest, gs.map)
-	when ""
-		if !p.last_move_dir.nil?
-			p.move(p.last_move_dir, gs.map)
-		else
-			puts "You stall in confusion."
-		end
-	else
-		puts "You stall in confusion."
-	end
-
-	if p.coord != coord_old
-		r = gs.rng.roll(100)
-		if r < 7
-			puts "You enter a battle!!!"
-			spawn_monsters(gs)
-			battle_loop(gs)
-		end
-	end
-end
+game_loop(gs)
--- src/state.rb
+++ src/state.rb
@@ -1,18 +1,23 @@
 require_relative 'affix'
 require_relative 'config'
 require_relative 'map'
+require_relative 'player'
 require_relative 'random'
 
 class GameState
+	attr_accessor :config
 	attr_accessor :map
+	attr_accessor :player
 	attr_accessor :monsters
 	attr_accessor :affix_db
 	attr_accessor :rng
 	attr_accessor :last_command
 	attr_accessor :last_battle_command
-	def initialize(config)
-		@map = GameMap.new(config.map)
-		@affix_db = AffixParserTransform.new.apply(parse_affix_db(config.affix_db))
+	def initialize(f)
+		@config = GameConfig.new(f)
+		@map = GameMap.new(@config.map)
+		@player = Player.new(@map.first_coord)
+		@affix_db = AffixParserTransform.new.apply(parse_affix_db(@config.affix_db))
 		@monsters = []
 		@rng = MWC256.new(:seed_manual, (1..258).to_a)
 		@last_command = ""
